66f17fab4c844f941f4204634e268830182838b3
zenbatista <harald.batista@yahoo.com>
2018-01-20 20:05:45 -0800

    Ternary Equation complete. Very cool.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -51,11 +51,7 @@ class TodoListViewController: UITableViewController {
         
         cell.textLabel?.text = item.title
         
-        if item.done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
+        cell.accessoryType = item.done  ? .checkmark : .none
         
         return cell
         
